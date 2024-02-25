0c8fbac9278108a480d9c4aaed2e63d42eee88ed
aayushk9 <ayushkokate1@gmail.com>
2024-02-25 20:27:21 +0530

    ignore files

--- server/app.js
+++ server/app.js
@@ -1,22 +1,33 @@
-const express = require('express');
-const path = require('path')
-const app = express();
+require('dotenv').config(); 
+const express = require('express');   
+const path = require('path');
+const app = express();     
+const config = require('./config');
+
 
 app.get('/', (req, res) => {
-    const filePath = path.join(__dirname, '../client/pages/home.html'); 
-    res.sendFile(filePath);   
+  const filePath = path.join(__dirname, '../client/pages/home.html');
+  const htmlContent = getHtmlContent(filePath, config.apiKey);
+  res.send(htmlContent);   
 })        
 
-app.get('/designing', (req, res) => {  
-  const filePath = path.join(__dirname, '../client/pages/design.html');
+app.get('/design', (req, res) => {  
+  const filePath = path.join(__dirname, '../client/pages/design.html');  
   res.sendFile(filePath);
 });
 
 app.get('/printing', (req, res) => {
   const filePath = path.join(__dirname, '../client/pages/printing.html');
-  req.sendFile(filePath);
+  res.sendFile(filePath);  
 });
 
-app.listen(3000, (req, res) => {
+function getHtmlContent(filePath, apiKey) {
+  const fs = require('fs');
+  let htmlContent = fs.readFileSync(filePath, 'utf-8');
+  htmlContent = htmlContent.replace('${process.env.GOOGLE_MAPS_API_KEY}', apiKey);
+  return htmlContent;
+}
+
+app.listen(3000, (req, res) => {   
     console.log('Server is listening on port 3000');
-})  
+});
